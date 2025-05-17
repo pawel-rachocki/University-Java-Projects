a0349f383858b266b5db12b986adefdea30f9349
Pawel <pawel.logo@gmail.com>
2025-05-17 16:26:23 +0200

    Added solution for LeetCode 26 problem

---
+++ LeetCode_26/src/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+
+        int index = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(index == 0){
+                nums[index] = nums[i];
+                index++;
+            }else if(nums[index - 1] != nums[i]){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
